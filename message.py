5a8c90207972b7b467fe0d3217e48cbf57d8faea
Muhammad Umer <80092235+mumer119131@users.noreply.github.com>
2022-02-15 08:30:36 +0500

    Update message.py

--- message.py
+++ message.py
@@ -2,11 +2,11 @@ from twilio.rest import Client
 
 
 def send_msg(data):
-    account_sid = 'AC346c543b7882650a028201a7ba76db0b'
-    auth_token = 'c7c78dec371a0d8118e94af522884fc5'
+    account_sid = '[sid]'
+    auth_token = '[key]'
     client = Client(account_sid, auth_token)
     message = client.messages.create(
         from_='whatsapp:+14155238886',
         body=data,
-        to='whatsapp:+923456013122'
+        to='whatsapp:[ph# no]'
     )
